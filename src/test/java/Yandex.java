e9855660a7800d780ee76e5626a5477ac5f69e4a
Иван <rzn.popov@gmail.com>
2020-04-05 23:54:11 +0300

    Initial commit

---
+++ src/test/java/Yandex.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.security.Key;
+import java.util.concurrent.TimeUnit;
+
+public class Yandex {
+    @Test
+    public void test() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        final Wait<WebDriver> wait = new WebDriverWait(driver, 20, 1000);
+        driver.get("https://translate.yandex.ru/?utm_source=wizard"); // Переходим по ссылке
+
+        // Находим элемент "Сайт", ждем, когда ссылка удет кликабельна
+        WebElement elemnt = driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/a[1]"));
+        wait.until(ExpectedConditions.elementToBeClickable((elemnt)));
+        elemnt.click();
+
+        // Открывается страница перевода сайта. Там находим поле для ввода ссылки. И затем вставляем ссылку.
+        WebElement url = driver.findElement(By.xpath("//*[@id=\"urlInput\"]"));
+        wait.until(ExpectedConditions.elementToBeClickable((url)));
+        url.sendKeys("https://gorest.co.in/");
+
+        // Находи кнопку "перевести", ждем когда она будет кликабельна. И нажимаем кнопку.
+        // Дальше откроется страница с переводом сайта
+        WebElement button = driver.findElement(By.xpath("/html/body/div[1]/div[4]/span[2]"));
+        wait.until(ExpectedConditions.elementToBeClickable((button)));
+        button.click();
+    }
+}
+
